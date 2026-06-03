78e1c5642eefd03a7179c6387f7a16aab6cb6700
dpnformateur-svg <dpn.formateur@gmail.com>
2026-06-03 14:21:34 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Service Worker – DPN Formation HACCP
+const CACHE_NAME = 'dpn-haccp-v1';
+const FILES_TO_CACHE = [
+  './Suivi_Formation_HACCP.html',
+  './manifest.json'
+];
+
+// Installation : mise en cache des fichiers
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[SW] Mise en cache des fichiers');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activation : nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Interception des requêtes : cache en priorité, réseau en fallback
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+      return fetch(event.request).then((networkResponse) => {
+        const responseClone = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return networkResponse;
+      }).catch(() => caches.match('./Suivi_Formation_HACCP.html'));
+    })
+  );
+});
